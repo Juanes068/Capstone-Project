333746692c1b4e48ba78ca860246ad9e58f69e2a
Juanes068 <trianajuan95@gmail.com>
2025-06-29 23:51:31 -0800

    Initial NOWA BARBER-SHOP commit

--- barber_backend/settings.py
+++ barber_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,8 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 ROOT_URLCONF = 'barber_backend.urls'
 
 TEMPLATES = [
