6b26343d09a4f245f88d7daa4a33740fc6b619a6
Juanes068 <trianajuan95@gmail.com>
2025-07-03 09:42:01 -0800

    ss

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from .models import Appointment, Barber, Service, Payment, Review
+from .models import Appointment
+from .models import Payment
+from .models import Barber
+from .models import Service
+
+from rest_framework import serializers
+from .models import Appointment, Barber, Service
 
 class AppointmentSerializer(serializers.ModelSerializer):
     barber = serializers.PrimaryKeyRelatedField(queryset=Barber.objects.all())
@@ -9,23 +15,31 @@ class AppointmentSerializer(serializers.ModelSerializer):
         model = Appointment
         fields = '__all__'
 
+
+
+
+
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
 
 class BarberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Barber
         fields = ['id', 'name', 'specialty', 'available']
 
+
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
-        fields = ['id', 'name', 'price']
+        fields = '__all__'
+
+from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
+    user = serializers.StringRelatedField(read_only=True) #swhos username
 
     class Meta:
         model = Review
